1ddbe642d45bc16c059773049e6cd2fb7b2bcc29
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-02-13 09:54:38 -0700

    Monday 0213

---
+++ lecture/functions.cpp
@@ -0,0 +1,67 @@
+/*
+Name: Jeremy Bergen
+
+Function and more functions
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// void sayHello() {
+//     cout << "Hello from sayHello new text" << endl;
+// }
+
+// float addNums(int num1, int num2) {
+//     float result;
+//     result = num1 + num2 + 5.5;
+//     return result;
+// }
+
+// void greetUser(string firstname) {
+//     cout << "Welcome " << firstname << " to my program." << endl;
+// }
+
+// string promptName() {
+//     string firstname;
+
+//     cout << "Please enter your first name: ";
+//     cin >> firstname;
+
+//     return firstname;
+// }
+
+void printNum(int num1) {
+    int num2;
+    cout << "inside printNum num1: " << &num1 << endl;
+    cout << "inside printNum num2: " << &num2 << endl;
+}
+
+int main() {
+    int num1;
+    int num2;
+    num1 = 42;
+    cout << "inside main num1: " << &num1 << endl;
+    cout << "inside main num2: " << &num2 << endl;
+    printNum(num1);
+    // string playerName;
+    // playerName = promptName();
+    // greetUser(playerName);
+    // greetUser("Bob");
+    // greetUser("Phil");
+
+    // int num1, bob;
+    // float addNumsAnswer;
+
+    // cout << "Please enter two numbers separated by a space: ";
+    // cin >> num1 >> bob;
+
+    // // DOES NOT WORK!!! cout << "The result of sayHello is: " << sayHello() << endl;
+    // addNumsAnswer = addNums(num1, bob);
+    // cout << "The numbers added together are: " << addNumsAnswer << endl;
+    // sayHello();
+    // sayHello();
+    // sayHello();
+
+    return 0;
+}
--- lecture/libraries.cpp
+++ lecture/libraries.cpp
@@ -4,11 +4,40 @@ Name: Jeremy Bergen
 Libraries
 */
 #include <iostream>
+#include <iomanip>
+#include <cstdio>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
+    string fullName;
+
+    cout << "Please enter your full name: ";
+    getline(cin, fullName);
+
+    cout << "Welcome, " << fullName << endl;
+    printf("Welcome, %s\n", fullName.c_str());
+    // float num1, num2;
+
+    // cout << "Please enter 2 numbers separated by a space: ";
+    // cin >> num1 >> num2;
+
+    // cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    // printf("%.2f + %.2f = %.2f\n", num1, num2, num1+num2);
+
+    // cout << setw(20) << "Num1" << setw(20) << "Num2" << endl;
+
+    // cout << setfill('-');
+    // cout << setw(40) << "-" << endl;
+    // cout << setfill(' ') << setprecision(10);
+    // cout << left << setw(20) << num1 << setw(20) << num2 << endl;
+
+    // char someChar;
+
+    // cout << "Please enter a number between 0-127: ";
+    // cin >> someChar;
+
+    // cout << "You entered: " << char(toupper(someChar)) << endl;
     // M_PI = pi basically...actually
 
     // float base, power;
