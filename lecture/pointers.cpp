c82b84a463e28e85807a786fd4a1ea1ae64e6a8b
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-03-08 09:49:44 -0700

    wednesday 0308

--- lecture/pointers.cpp
+++ lecture/pointers.cpp
@@ -8,32 +8,120 @@ Pointers
 
 using namespace std;
 
+int addNums(int*, int*);
+
 //int main(int argc, char **argv)
 int main(int argc, char *argv[]) {
-    int num1, num2, num3, num4;
-    int *ptr1;
+    int n1, n2;
+    int result;
+    int *ptr1, *ptr2;
 
-    num1 = 42;
-    num2 = 15;
-    num3 = 9000;
-    num4 = -5;
+    n1 = 42;
+    n2 = 15;
+    // ptr1 = &n1;
+    ptr2 = &n2;
 
-    ptr1 = &num1;
+    result = addNums(&n1, ptr2);
 
-    cout << "num1: " << num1 << endl;
-    cout << "*ptr1: " << *ptr1 << endl;
+    cout << n1 << " + " << n2 << " = " << result << endl;
+    return 0;
+}
 
-    ptr1 = ptr1 + 1;
+int addNums(int *num1, int* num2) {
+    return *num1 + *num2;
+}
 
-    cout << "*ptr1: " << *ptr1 << endl;
 
-    ptr1 = ptr1 + 1;
 
-    cout << "*ptr1: " << *ptr1 << endl;
 
-    ptr1 = ptr1 + 1;
+    // int num1, num2, num3, num4;
+    // int *ptr1;
 
-    cout << "*ptr1: " << *ptr1 << endl;
+    // num1 = 42;
+    // num2 = 15;
+    // num3 = 9000;
+    // num4 = -92;
+
+    // ptr1 = &num1;
+
+    // cout << "ptr1: " << ptr1 << endl;
+    // cout << "*ptr1: " << *ptr1 << endl;
+
+    // // ptr1++;
+    // ptr1 += 3;
+
+    // cout << "ptr1: " << ptr1 << endl;
+    // cout << "*ptr1: " << *ptr1 << endl;
+
+    // ptr1++;
+
+    // cout << "ptr1: " << ptr1 << endl;
+    // cout << "*ptr1: " << *ptr1 << endl;
+
+    // ptr1++;
+
+    // cout << "ptr1: " << ptr1 << endl;
+    // cout << "*ptr1: " << *ptr1 << endl;
+
+    // ptr1--;
+
+    // cout << "ptr1: " << ptr1 << endl;
+    // cout << "*ptr1: " << *ptr1 << endl;
+
+    // long num1;
+    // long * somePointer1 = nullptr;
+    // //long *somePointer1 = NULL;
+
+    // num1 = 42;
+    // // somePointer1 = &num1;
+    // if (somePointer1 == NULL) {
+    //     somePointer1 = &num1;
+    // }
+
+    // // cout << "num1: " << &num1 << endl;
+    // cout << "*somePointer1: " << *somePointer1 << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int num1, num2, num3, num4;
+    // int *ptr1;
+
+    // num1 = 42;
+    // num2 = 15;
+    // num3 = 9000;
+    // num4 = -5;
+
+    // ptr1 = &num1;
+
+    // cout << "num1: " << num1 << endl;
+    // cout << "*ptr1: " << *ptr1 << endl;
+
+    // ptr1 = ptr1 + 1;
+
+    // cout << "*ptr1: " << *ptr1 << endl;
+
+    // ptr1 = ptr1 + 1;
+
+    // cout << "*ptr1: " << *ptr1 << endl;
+
+    // ptr1 = ptr1 + 1;
+
+    // cout << "*ptr1: " << *ptr1 << endl;
 
     // int num1 = 0;
     // float fnum1;
@@ -74,5 +162,3 @@ int main(int argc, char *argv[]) {
     // cout << "&ptr2: " << &ptr2 << endl;
     // cout << "&ptr3: " << &ptr3 << endl;
 
-    return 0;
-}
