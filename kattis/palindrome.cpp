4b842de14496521fd40ba474dc4dd155dddf4955
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-04-04 22:01:02 -0600

    wednesday 0405

--- kattis/palindrome.cpp
+++ kattis/palindrome.cpp
@@ -11,25 +11,40 @@ using namespace std;
 int main(int argc, char *argv[]) {
     string inputString;
     string sanitizedString = "";
-    string reverseString = "";
+    // string reverseString = "";
+    bool isPalindrome = true;
 
     cout << "Enter a string to check if it's a palindrome: ";
     getline(cin, inputString);
 
     //Get rid of spaces in inputString
     for (size_t i = 0; i < inputString.length(); i++) {
-        if (inputString.at(i) != ' ') {
-            sanitizedString += inputString.at(i);
+        // if (inputString.at(i) != ' ' || inputString.at(i) != ',' || inputString.at(i) != '.') {
+        if((inputString.at(i) >= 65 && inputString.at(i) <= 90) || (inputString.at(i) >= 'a' && inputString.at(i) <= 'z')) {
+            sanitizedString += tolower(inputString.at(i));
         }
     }
 
-    for(int i = sanitizedString.length()-1; i >= 0; i--) {
-        // reverseString.append((string)inputString[i]);
-        reverseString += sanitizedString.at(i);
+    // for(int i = sanitizedString.length()-1; i >= 0; i--) {
+    //     // reverseString.append((string)inputString[i]);
+    //     reverseString += sanitizedString.at(i);
+    // }
+    size_t sanStrLength = sanitizedString.length()-1;
+    for(size_t i = 0; i < sanitizedString.length()/2; i++) {
+        cout << "i: " << i << endl;
+        
+        if(sanitizedString.at(i) != sanitizedString.at(sanStrLength-i)) {
+            isPalindrome = false;
+            break;
+        }
+
+        // sanStrLength--;
     }
 
-    if (sanitizedString == reverseString) {
+    if (isPalindrome) {
         cout << "Your string is a palindrome!" << endl;
+    } else {
+        cout << "Your string is NOT a palindrome!" << endl;
     }
     // cout << "DEBUG: inputString:\t" << inputString << endl;
     // cout << "DEBUG: reverseString:\t" << reverseString << endl;
