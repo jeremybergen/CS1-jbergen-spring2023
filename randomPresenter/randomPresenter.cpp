b800d8e01a7fe35b2fe1856270e4b5da81fb5b61
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-05-09 09:51:38 -0600

    tuesday 0509

--- randomPresenter/randomPresenter.cpp
+++ randomPresenter/randomPresenter.cpp
@@ -18,28 +18,33 @@ struct Presenter {
 
 void countLines(ifstream&, int&);
 void readData(ifstream&, Presenter[], int&);
-void randPresenter();
-void printPresenter();
-void writeData();
+void randPresenter(Presenter[], int&, int&);
+void printPresenter(Presenter&);
+void writeData(int&, Presenter[], int, ofstream&);
 
 int main(int argc, char *argv[]) {
+    srand(time(0));
+
     int arrSize = 0;
+    int randVictim = 0;
     ifstream fin;
+    ofstream fout;
     fin.open("names.csv");
-    cout << "DEBUG: rdstate(): " << fin.rdstate() << endl;
-    cout << boolalpha << "DEBUG: good(): " << fin.good() << endl;
-    cout << boolalpha << "DEBUG: eof(): " << fin.eof() << endl;
-    cout << boolalpha << "DEBUG: fail(): " << fin.fail() << endl;
-    cout << boolalpha << "DEBUG: bad(): " << fin.bad() << endl;
+    
+    // cout << "DEBUG: rdstate(): " << fin.rdstate() << endl;
+    // cout << boolalpha << "DEBUG: good(): " << fin.good() << endl;
+    // cout << boolalpha << "DEBUG: eof(): " << fin.eof() << endl;
+    // cout << boolalpha << "DEBUG: fail(): " << fin.fail() << endl;
+    // cout << boolalpha << "DEBUG: bad(): " << fin.bad() << endl;
 
     // cout << "DEBUG: fin.tellg(): " << fin.tellg() << endl;
     countLines(fin, arrSize);
-    cout << "After countLines" << endl;
-    cout << "DEBUG: rdstate(): " << fin.rdstate() << endl;
-    cout << boolalpha << "DEBUG: good(): " << fin.good() << endl;
-    cout << boolalpha << "DEBUG: eof(): " << fin.eof() << endl;
-    cout << boolalpha << "DEBUG: fail(): " << fin.fail() << endl;
-    cout << boolalpha << "DEBUG: bad(): " << fin.bad() << endl;
+    // cout << "After countLines" << endl;
+    // cout << "DEBUG: rdstate(): " << fin.rdstate() << endl;
+    // cout << boolalpha << "DEBUG: good(): " << fin.good() << endl;
+    // cout << boolalpha << "DEBUG: eof(): " << fin.eof() << endl;
+    // cout << boolalpha << "DEBUG: fail(): " << fin.fail() << endl;
+    // cout << boolalpha << "DEBUG: bad(): " << fin.bad() << endl;
     // fin.seekg(0, fin.beg);
     // fin.close();
     // fin.open("names.csv");
@@ -52,25 +57,56 @@ int main(int argc, char *argv[]) {
 
     readData(fin, presenters, arrSize);
 
-    for(int i = 0; i < arrSize; i++) {
-        cout << "DEBUG: " << presenters[i].fName << " " << presenters[i].lName << endl;
-    }
+    // for(int i = 0; i < arrSize; i++) {
+    //     cout << "DEBUG: " << presenters[i].fName << " " << presenters[i].lName << endl;
+    // }
+
+    randPresenter(presenters, arrSize, randVictim);
+    printPresenter(presenters[randVictim]);
 
     fin.close();
+    fout.open("names.csv");
+
+    writeData(randVictim, presenters, arrSize, fout);
+
+    fout.close();
     return 0;
+}
+
+void writeData(int& randVictim, Presenter presenters[], int arrCounter, ofstream& fout) {
+    for(int i = 0; i < arrCounter; i++) {
+        // if(i == randVictim) {
+        //     continue;
+        // }
+        if(i != randVictim) {
+            fout << presenters[i].lName << ", " << presenters[i].fName << endl;
+        }
+    }
+}
+
+void printPresenter(Presenter& randPresenter) {
+    cout << "The next victim, I mean presenter, is....drumroll (don't say drumroll): " 
+         << endl << randPresenter.fName << " " << randPresenter.lName << endl;
+}
+
+void randPresenter(Presenter presenters[], int& arrCounter, int& randVictim) {
+    // cout << "DEBUG: arrCounter: " << arrCounter << endl;
+    randVictim = random() % arrCounter;
+    // cout << "DEBUG: randVictim: " << randVictim << endl;
 }
 
 void countLines(ifstream& fin, int& numLines){
     string tmpBuf;
     numLines = 0;
-    cout << "DEBUG: fin.tellg(): " << fin.tellg() << endl;
+    // cout << "DEBUG: fin.tellg(): " << fin.tellg() << endl;
 
     while(getline(fin, tmpBuf)) {
         numLines++;
     }
+    // cout << "DEBUG: numLines: " << numLines << endl;
     fin.clear();
     fin.seekg(0);
-    cout << "DEBUG: fin.tellg(): " << fin.tellg() << endl;
+    // cout << "DEBUG: fin.tellg(): " << fin.tellg() << endl;
 
     
     // cout << "DEBUG: fin.tellg(): " << fin.tellg() << endl;
